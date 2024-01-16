c5ef5f6ecc49dd42c64b9cd464746d568b7f742f
ArchieR7 <Archie@Archie.tw>
2024-01-16 16:57:07 +0800

    fix(contracts): fix import path

--- EVM/contracts/test/TestNFT.sol
+++ EVM/contracts/test/TestNFT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../tokens/ERC6672.sol";
+import "../token/ERC6672.sol";
 
 contract TestNFT is ERC6672, ERC721Burnable, Ownable {
     uint256 _tokenIdCounter;
--- EVM/contracts/token/ERC6672.sol
+++ EVM/contracts/token/ERC6672.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC721, IERC165} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import {IERC6672} from "../interfaces/IERC6672.sol";
+import {IERC6672} from "../interface/IERC6672.sol";
 
 abstract contract ERC6672 is ERC721, ERC721Enumerable, IERC6672 {
     error ERC6672Unredeemable(
